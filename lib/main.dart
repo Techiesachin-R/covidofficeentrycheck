a004dfe13b1496b9255a25b8a052015e63caf6ae
sachinensurity <sachin.r@ensurity.com>
2020-06-11 16:02:31 +0530

    logic code added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -26,131 +28,210 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var arr = new List(5);
-  var arrno = new List(5);
-
-List<Question> _items = [
+  List yesarray = <String>[];
+  List noarray = <String>[];
+  bool pressAttention = false;
+  List<Question> _items = [
     Question(
-      id: 'y1',
+      id: 1,
       title: 'whats your name?',
+      upclick:false,
+      downclick:false
     ),
     Question(
-      id: 'y2',
+      id: 2,
       title: 'which food you like most?',
+            upclick:false,
+      downclick:false
     ),
     Question(
-      id: 'y3',
+      id: 3,
       title: 'Where you want to go?',
+            upclick:false,
+      downclick:false
     ),
     Question(
-      id: 'y4',
+      id: 4,
       title: 'Where you want to sdsd?',
+            upclick:false,
+      downclick:false
     ),
     Question(
-      id: 'y5',
+      id: 5,
       title: 'Where you want to go?',
+      upclick:true,
+      downclick:false
     ),
   ];
 
-
-
-
-  // void _incrementyesCounter() {
-  //   setState(() {
-  //     _yes ++;
-  //     this.pressAttention = false;
-  //     this.arr.add('yes');
-  //   });
-  // }
-
-  // void _incrementnoCounter() {
-  //   setState(() {
-  //     _no ++;
-  //   });
-  // }
-
-  void pressedyes(data){
-      this.arr.add(data);
-      print(this.arr);
+  void pressedyes(data) {
+    if (this.noarray.contains(data.toString())) {
+      this.noarray.remove(data.toString());
+      this.yesarray.add(data.toString());
+      this.yesarray.toSet();
+    } else {
+      this.yesarray.add(data.toString());
+      this.yesarray.toSet();
+    }
+    print('yesarray:  + ${this.yesarray.toSet()}');
+    print('noarray:  + ${this.noarray.toSet()}');
+    print('id : ${_items[data].upclick}');
   }
 
-  void pressedno(data){
-      this.arr.add(data);
-      print(this.arr);
+  void pressedno(data) {
+    if (this.yesarray.contains(data.toString())) {
+      this.yesarray.remove(data.toString());
+      this.noarray.add(data.toString());
+      this.noarray.toSet();
+    } else {
+      this.noarray.add(data.toString());
+      this.noarray.toSet();
+    }
+    print('yesarray:  + ${this.yesarray.toSet()}');
+    print('noarray:  + ${this.noarray.toSet()}');
   }
+
+  //Todo ENABLE SUBMIT BUTTON BASED ON CONDITION ALL FILLED 
+  void submit(){
+
+
+      if(this.yesarray.length == 5){
+        print('object');
+        _showMyDialog(yesarray.toSet().length.toString());
+      }else{
+         _showMyDialog(yesarray.toSet().length.toString());
+        print('sdfsd');
+      }
+      //Todo empty arrays and object and refresh 
+  }
+
+  Future<void> _showMyDialog(response) async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('You are In'),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Text(response),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Okay'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Image.asset('assets/logo.png', fit: BoxFit.cover,
-        width: 120.0,),
-      ),
-      body: Container(
-        color: const Color(0x111B5800),
-        child: ListView.builder(
-          itemCount: _items.length,
-          itemBuilder: (context, index) {
-          return  Card(
-              child: Container(
-                child: Row(
-                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                 crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.all(15.0),
-                      child: Text('${_items[index].title}',
-                      style: TextStyle(
-                        fontSize: 20.0
-                      ),)),
-                    Container(
-                      child: ButtonBar(
-                        children: <Widget>[
-                          IconButton(
-                            icon: Icon(
-                              Icons.thumb_up,
-                            ),
-                            iconSize: 30,
-                            color: Colors.grey,
-                            onPressed: (){
-                              pressedyes(_items[index].id);
-                            },
+        appBar: AppBar(
+          title: Image.asset(
+            'assets/logo.png',
+            fit: BoxFit.cover,
+            width: 120.0,
+          ),
+        ),
+        body: Container(
+          color: const Color(0x111B5800),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              new Expanded(
+                    child: ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    primary: true,
+                    itemCount: _items.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Container(
+                                  padding: EdgeInsets.all(15.0),
+                                  child: Text(
+                                    '${_items[index].title}',
+                                    style: TextStyle(fontSize: 20.0),
+                                  )),
+                              Container(
+                                child: ButtonBar(
+                                  children: <Widget>[
+                                    IconButton(
+                                      icon: Icon(
+                                        Icons.thumb_up,
+                                      ),
+                                      iconSize: 30,
+                                      //color: 'pressAttention-${_items[index].id.toString()}' ? Colors.grey : Colors.blue,
+                                      color: Colors.grey,
+                                      onPressed: () {
+                                        pressedyes(_items[index].id);
+                                        setState(() {
+                                          
+                                        });
+                                      },
+                                    ),
+                                    IconButton(
+                                      icon: Icon(
+                                        Icons.thumb_down,
+                                      ),
+                                      iconSize: 30,
+                                      color: Colors.grey,
+                                      onPressed: () {
+                                        pressedno(_items[index].id);
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
                           ),
-                          IconButton(
-                            icon: Icon(
-                              Icons.thumb_down,
-                            ),
-                            iconSize: 30,
-                            color:Colors.grey,
-                            onPressed: (){
-                              pressedno(_items[index].id);
-                            },
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                        ),
+                      );
+                    }),
               ),
-            );
-          
-          // ListTile(
-          //   title: Text('${_items[index].title}'),
-          // );
-          }
-        )
-      ));
+              FlatButton(
+              color: Colors.blue,
+                onPressed: (){
+                      submit();
+                },
+                child: Text('Submit',
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white
+              ),
+              
+              ),
+              )
+            ],
+          ),
+        ));
   }
 }
 
-
 class Question {
-  final String id;
+  final int id;
   final String title;
-
+  final bool upclick;
+  final bool downclick;
 
   Question({
     @required this.id,
     @required this.title,
+    @required this.upclick,
+    @required this.downclick,
   });
 }
